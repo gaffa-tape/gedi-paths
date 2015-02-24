28735b98996e06d2272f5c42f3ededb8a7f68c32
Kory Nunn <knunn187@gmail.com>
2015-02-24 19:11:43 +1000

    regex versin

--- detectPath.js
+++ detectPath.js
@@ -1,18 +1,6 @@
-module.exports = function detectPath(substring){
-    if (substring.charAt(0) === '[') {
-        var index = 1;
+var matchPath = /^\[(?:[^\[\]]|\\.)*\]/;
 
-        do {
-            if (
-                (substring.charAt(index) === '\\' && substring.charAt(index + 1) === '\\') || // escaped escapes
-                (substring.charAt(index) === '\\' && (substring.charAt(index + 1) === '[' || substring.charAt(index + 1) === ']')) //escaped braces
-            ) {
-                index++;
-            }
-            else if(substring.charAt(index) === ']'){
-                return substring.slice(0, index+1);
-            }
-            index++;
-        } while (index < substring.length);
-    }
+module.exports = function detectPath(substring){
+    var match = substring.match(matchPath);
+    return match && match[0];
 };
